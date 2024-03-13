4695a9b8dc548a45c323d611d4b5d69713defa82
abubienczyk <olabubienczyk@gmail.com>
2024-03-13 12:22:58 +0100

    git commit -m "Przyjmij tablice intów"

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -5,3 +5,5 @@ Console.WriteLine("Modyfikacja 1");
 Console.WriteLine("Modyfikacja 2");
 Console.WriteLine("Modyfikacja 3");
 Console.WriteLine("Modyfikacja 4");
+
+Console.WriteLine("przyjmij tablice");
